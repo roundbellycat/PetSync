bd500c43da4c72f3e6993080c51d0afc053651a6
jennii <jnjenniingo@gmail.com>
2025-08-16 12:10:08 +1000

    updated baseurl

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.104.79.85:5001', // live
+  baseURL: 'http://16.176.172.187:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
